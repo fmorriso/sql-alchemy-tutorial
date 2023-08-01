5f00d70e2b9a21fe5603f416c1462d301b1b7c41
Frederick Morrison <frederick.morrison@outlook.com>
2023-08-01 16:48:59 -0600

    add two ways to COMMIT

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def get_python_version() -> str:
 def get_sqlalchemy_version() -> str:
     return sqlalchemy.__version__
 
+
 def display_hello_world(engine: Engine):
     with engine.connect() as conn:
         result: CursorResult = conn.execute(text("select 'hello world'"))
@@ -29,6 +30,26 @@ def display_hello_world(engine: Engine):
         # print(type(txt))
         print(txt)
 
+
+def commit_as_you_go(engine: Engine):
+    with engine.connect() as conn:
+        conn.execute(text("CREATE TABLE some_table (x int, y int)"))
+        result: CursorResult = conn.execute(
+            text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
+            [{"x": 1, "y": 1}, {"x": 2, "y": 4}],
+        )
+        conn.commit()
+
+
+def use_transaction_to_commit(engine: Engine):
+    """"Assumes commit_as_you_go has been called previously"""
+    with engine.begin() as conn:
+        result: CursorResult = conn.execute(
+            text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
+            [{"x": 6, "y": 8}, {"x": 9, "y": 10}],
+        )
+
+
 if __name__ == "__main__":
     print(f"python version: {get_python_version()}")
     print(f"SQLAlchemy version: {get_sqlalchemy_version()}")
@@ -37,3 +58,5 @@ if __name__ == "__main__":
     # print(type(engine))
 
     display_hello_world(engine)
+    commit_as_you_go(engine)
+    use_transaction_to_commit(engine)
